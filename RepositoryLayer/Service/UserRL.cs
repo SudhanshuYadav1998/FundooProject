960797c8914fbff18107e4cfd2c02eb456fc80d9
SudhanshuYadav1998 <sy7040@gmail.com>
2022-10-14 15:56:15 +0530

    added. Encryption and Decryption Password for User login and registration

--- RepositoryLayer/Service/UserRL.cs
+++ RepositoryLayer/Service/UserRL.cs
@@ -142,6 +142,7 @@ namespace RepositoryLayer.Service
                 Rfc2898DeriveBytes pdb = new Rfc2898DeriveBytes(EncryptionKey, new byte[] {
             0x49, 0x76, 0x61, 0x6e, 0x20, 0x4d, 0x65, 0x64, 0x76, 0x65, 0x64, 0x65, 0x76
         });
+               
                 encryptor.Key = pdb.GetBytes(32);
                 encryptor.IV = pdb.GetBytes(16);
                 using (MemoryStream ms = new MemoryStream())
